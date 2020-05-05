79d2cf7e195de06bbd38c0578f1289d45cbb2e58
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-05-05 15:14:55 +0400

    added initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@material-ui/core';
+
+import './App.scss';
+import { Header } from './components/Header';
+import { Content } from './components/Content';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container maxWidth="lg">
+          <Header/>
+          <Content />
+          <Footer />
+      </Container>
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>Your header</h1>
+        </header>
+    );
+};

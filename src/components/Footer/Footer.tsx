cf2219ec1ba4b691f548ebe78ef3b6b1d6553d72
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-05-06 16:25:21 +0400

    played with webpack

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export const Footer = () => {
-    return (
-      <footer>
-          Footer
-      </footer>
-    );
+  return <footer>Footer</footer>;
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { StyledHeader } from './styled';
 
 export const Header = () => {
-    return (
-        <StyledHeader>
-            <h1>Names List App</h1>
-        </StyledHeader>
-    );
+  return (
+    <StyledHeader>
+      <h1>Names List App</h1>
+    </StyledHeader>
+  );
 };
